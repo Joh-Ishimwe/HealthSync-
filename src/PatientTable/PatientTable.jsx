7bfda0a6554b5cc91a537c08211956e74ec908fe
Samuel Dushime <s.dushime@alustudent.com>
2024-04-10 10:33:14 +0200

    done

--- src/PatientTable/PatientTable.jsx
+++ src/PatientTable/PatientTable.jsx
@@ -26,14 +26,7 @@ function Labtable() {
   return (
     <div className="w-[970px] bg-white px-5 pb-5 shadow-xl rounded-lg mt-5">
       <div className="flex justify-between items-center pt-3 pb-3">
-        <h1 className="text-[15px] font-bold">Labs</h1>
-        <div>
-          <a href="">
-            <button className="bg-[#00afee] w-[100px] text-[11px] h-[30px] hover:bg-[#00306a] text-white font-bold py-2 px-4 rounded">
-              View All
-            </button>
-          </a>
-        </div>
+        
       </div>
       <table className="min-w-full">
         <thead>
@@ -50,7 +43,7 @@ function Labtable() {
           {LabExams.map((exam, index) => (
             <tr
               key={index}
-              className={`text-[11px] font-bold h-[34px] ${
+              className={`text-[11px]  h-[34px] ${
                 index % 2 === 0 ? "bg-[#ddf4fc]  " : ""
               }`}
             >
